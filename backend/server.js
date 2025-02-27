f284b72391bb47bc81ed43c379c3882b211b325d
hariskhn <harislif3@gmail.com>
2025-02-27 21:55:26 +0500

    Add cookie parser middleware and update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+
 import connectDB from "./lib/db.js";
 
 dotenv.config();
@@ -8,11 +11,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); 
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
-
-app.use("/api/v1/auth", authRoutes);
