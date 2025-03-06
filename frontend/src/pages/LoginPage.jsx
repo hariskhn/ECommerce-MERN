65245eb0fe3181a0714f17d1d1c897fe92a352d6
hariskhn <harislif3@gmail.com>
2025-03-07 02:45:30 +0500

    Refactor Login and SignUp pages to integrate user authentication with Zustand store

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore";
 
 const LoginPage = () => {
-  const loading = false;
-
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	const { login, loading } = useUserStore();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(email, password);
+		login( email, password )
 	};
 
 	return (
@@ -23,7 +23,7 @@ const LoginPage = () => {
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.8 }}
 			>
-				<h2 className='mt-6 text-center text-3xl font-extrabold text-emerald-400'>Create your account</h2>
+				<h2 className='mt-6 text-center text-3xl font-extrabold text-emerald-400'>Login</h2>
 			</motion.div>
 
 			<motion.div
