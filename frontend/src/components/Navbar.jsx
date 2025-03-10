4ddebffec044a710f7c68146b842bd428ec5336d
hariskhn <harislif3@gmail.com>
2025-03-11 04:38:27 +0500

    Update Navbar to display dynamic cart item count

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
     const { user, logout } = useUserStore();
+    const { cart } = useCartStore();
     const isAdmin = user?.role === "admin";
 
     return (
@@ -30,12 +32,12 @@ const Navbar = () => {
                             >
                                 <ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
                                 <span className='hidden sm:inline'>Cart</span>
-                                {3 > 0 && (
+                                {cart.length > 0 && (
                                     <span
                                         className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
                                     >
-                                        3
+                                        {cart.length}
                                     </span>
                                 )}
                             </Link>
