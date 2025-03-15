320dbab9125036954605fba6aaf31031ec25671f
hariskhn <harislif3@gmail.com>
2025-03-16 01:33:45 +0500

    Configured CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors"
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -16,6 +17,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use (cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
 
 const __dirname = path.resolve();
 
